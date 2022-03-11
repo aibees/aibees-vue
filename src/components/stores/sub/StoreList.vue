3bf2155b23b1d8b3bab8de8fec026b9b1ee6d637
aibees <aibees1129@naver.com>
2022-03-11 23:44:21 +0900

    2022.03.11

--- src/components/stores/sub/StoreList.vue
+++ src/components/stores/sub/StoreList.vue
@@ -10,15 +10,18 @@
             <div class="product_left">
                 <h3 class="name">{{ data.name }}</h3>
                 <div class="amounts">
-                    공급가 : {{data.buyPrice}}원 / 택배비 : {{data.buyCourier}}원 - 판매가 : {{data.selPrice}}원 / 판매택배비 : {{data.selCourier}}원
+                  공급가 : {{data.buyPrice}}원 / 구매택배비 : {{data.buyCourier}}원 <br/> 판매가 : {{data.selPrice}}원 / 판매택배비 : {{data.selCourier}}원
                 </div>
             </div>
             <div class="product_right">
                 <p> 마진율 : {{ data.marginRate }}% </p>
                 <p> 마진액 : {{ data.marginAmount }} </p>
                 <p>
-                  <a :href="data.hlink" target="_blank"><h3 style="margin-bottom: 5px;">주문하러 가기</h3></a>
-                  <a :href="data.hlink"><h3 style="margin-bottom: 5px;">수정하기</h3></a>
+                  <a :href="data.hlink" target="_blank">주문하러가기</a> / 
+                  <a :href="data.hlink">수정하기</a> / 
+                  <a :href="data.hlink">삭제</a> 
+                  <!-- <a :href="data.hlink" target="_blank"><h3 style="margin-bottom: 5px;">주문하러 가기</h3></a>
+                  <a :href="data.hlink"><h3 style="margin-bottom: 5px;">수정하기</h3></a>/<a :href="data.hlink"><h3 style="margin-bottom: 5px;">삭제</h3></a> -->
                 </p>
             </div>
         </div>
@@ -54,6 +57,26 @@
             { name: '케어올 FK94 마스크 100매',
            tags: ['생활/건강', '수납'],
            platform: '쿠팡',
+            buyPrice: 12000,
+            buyCourier: 3500,
+            selPrice: 19000,
+            selCourier: 0,
+            marginRate: 10.4,
+            marginAmount: 3500,
+            hlink: 'http://domeggook.com/19211492?from=lstGen/'  },
+            { name: '수납 언더베드',
+           tags: ['생활/건강', '수납'],
+           platform: '네이버',
+            buyPrice: 12000,
+            buyCourier: 3500,
+            selPrice: 19000,
+            selCourier: 0,
+            marginRate: 10.4,
+            marginAmount: 3500,
+            hlink: 'http://domeggook.com/19211492?from=lstGen/'  },
+            { name: '수납 언더베드',
+           tags: ['생활/건강', '수납'],
+           platform: '네이버',
             buyPrice: 12000,
             buyCourier: 3500,
             selPrice: 19000,
---
+++ src/components/stores/sub/StoreSearch.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="search-tab">
+        여기에 검색 탭 들어갈거에요
+        <button > 검색 </button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+    @import '@@/store/sub/store_search.scss'
+</style>
